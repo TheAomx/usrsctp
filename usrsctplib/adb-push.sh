f220c42937bcc8d366fdeac5f69cde78a5ab84f7
Julian Cordes <julian.cordes@gmail.com>
2016-01-01 22:20:18 +0100

    adjustments to get usrsctp lib working with android 19

---
+++ usrsctplib/adb-push.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+adb push .libs/libusrsctp.a /data/local/tmp/
+adb push .libs/libusrsctp.la /data/local/tmp/
+adb push .libs/libusrsctp.so /data/local/tmp/
+adb push .libs/libusrsctp.so.1 /data/local/tmp/
+adb push .libs/libusrsctp.so.1.0.0 /data/local/tmp/
+
