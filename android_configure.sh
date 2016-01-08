7ffe44110a07e31b9223d6309d3048edccffc9c2
Julian Cordes <julian.cordes@gmail.com>
2016-01-08 15:21:43 +0100

    cleaned up android_configure file

--- android_configure.sh
+++ android_configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# I put all my dev stuff in here
-export DEV_PREFIX=$HOME/git/sctpwithjava/jni-src
+# This is just an empty directory where I want the built objects to be installed
+export PREFIX=/home/aomx/git/usrsctp/android_libs
 
 # Don't forget to adjust this to your NDK path
 export ANDROID_NDK=/home/aomx/android-ndk-r10e
@@ -12,7 +12,7 @@ export CROSS_COMPILE=arm-linux-androideabi
 export ANDROID_PREFIX=${ANDROID_NDK}/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
 
 # Apparently android-8 works fine, there are other versions, look them up
-export SYSROOT=${ANDROID_NDK}/platforms/android-19/arch-arm
+export SYSROOT=${ANDROID_NDK}/platforms/android-8/arch-arm
 
 export CROSS_PATH=${ANDROID_PREFIX}/bin/${CROSS_COMPILE}
 
@@ -28,7 +28,7 @@ export LD=${CROSS_PATH}-ld
 export RANLIB=${CROSS_PATH}-ranlib
 
 # This is just an empty directory where I want the built objects to be installed
-export PREFIX=${DEV_PREFIX}/android_build/
+export PREFIX=/home/aomx/git/usrsctp/android_libs
 
 # Don't mix up .pc files from your host and build target
 export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
@@ -36,8 +36,9 @@ export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
 # You can clone the full Android sources to get bionic if you want.. I didn't
 # want to so I just got linker.h from here: http://gitorious.org/0xdroid/bionic
 # Note that this was only required to build boehm-gc with dynamic linking support.
-export CFLAGS="${CFLAGS} --sysroot=${SYSROOT} -I${SYSROOT}/usr/include -I${ANDROID_PREFIX}/include -Ofast -fPIE -pie"
+export CFLAGS="${CFLAGS} --sysroot=${SYSROOT} -I${SYSROOT}/usr/include -I${ANDROID_PREFIX}/include"
 export CPPFLAGS="${CFLAGS}"
+export CXXFLAGS="${CFLAGS}"
 export LDFLAGS="${LDFLAGS} -L${SYSROOT}/usr/lib -L${ANDROID_PREFIX}/lib"
 
 ac_cv_c_bigendian=no ./configure --host=${CROSS_COMPILE} --with-sysroot=${SYSROOT} --prefix=${PREFIX} "$@"
